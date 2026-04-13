21c531978556bfb765f4b014868c49177a73884d
Exclude0122 <173652916+Exclude0122@users.noreply.github.com>
2026-04-13 14:59:14 -0400

    add donation

--- app/src/main/java/cn/gov/xivpn2/ui/SettingsFragment.java
+++ app/src/main/java/cn/gov/xivpn2/ui/SettingsFragment.java
@@ -1,6 +1,8 @@
 package cn.gov.xivpn2.ui;
 
 import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,15 +10,19 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+
+import java.util.Objects;
 
 import cn.gov.xivpn2.BuildConfig;
 import cn.gov.xivpn2.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
@@ -60,6 +66,32 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         findPreference("backup_or_restore").setOnPreferenceClickListener(preference -> {
             startActivity(new Intent(getContext(), BackupActivity.class));
+            return true;
+        });
+
+        findPreference("donation").setOnPreferenceClickListener(preference -> {
+            new AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.donation)
+                    .setItems(R.array.donation, (dialog, which) -> {
+                        String walletAddress = "";
+                        switch (which) {
+                            case 0: walletAddress = "TTpzvVJ7cv2RZVihd48GGZXg1896WFgQuJ"; break;
+                            case 1: walletAddress = "0x593065aDE108505356abaD9c58bE950115678593"; break;
+                            case 2: walletAddress = "CyRPKfkGnrAtVKijorcFYocL5fY37tX9j1atm2m8cY8m"; break;
+                            case 3: walletAddress = "84iR4Tz29wFKxDpceeFhZQc3msh7N59PdNqxhEY9HjtZKs7wHqGLhw5AJ5p5zkxHMpU7DKHmhjjHmV7jaoVteoWsQs81tf3"; break;
+                        }
+
+                        String finalWalletAddress = walletAddress;
+                        new AlertDialog.Builder(requireContext())
+                                .setTitle(R.string.wallet_address)
+                                .setMessage(walletAddress)
+                                .setPositiveButton(R.string.copy, (dialog1, which1) -> {
+                                    ClipboardManager clipboardManager = requireContext().getSystemService(ClipboardManager.class);
+                                    clipboardManager.setPrimaryClip(ClipData.newPlainText("Wallet Address", finalWalletAddress));
+                                })
+                                .show();
+                    })
+                    .show();
             return true;
         });
     }
